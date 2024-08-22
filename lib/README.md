b8da5f6e6a9b2578818603785f6a1a5234a4fde7
Britt Cyr <cyrbritt@gmail.com>
2024-08-22 15:52:37 -0400

    Data structures for manifest (#1)
    * Data structures
    
    * readme comment
    
    * fmt
    
    * restore gpl
    
    * rename

---
+++ lib/README.md
@@ -0,0 +1,5 @@
+# Hypertree
+
+Hypertree is a library for creating efficient data structures on solana that do
+not own the underlying byte array. This special feature allows overlapping data
+structures within the same region of memory, inside of the same account.
---
+++ lib/src/utils.rs
@@ -0,0 +1,52 @@
+use std::mem::size_of;
+
+pub type DataIndex = u32;
+
+/// Read a struct of type T in an array of data at a given index.
+pub fn get_helper<T: bytemuck::Pod>(data: &[u8], index: DataIndex) -> &T {
+    let index_usize: usize = index as usize;
+    bytemuck::from_bytes(&data[index_usize..index_usize + size_of::<T>()])
+}
+
+/// Read a struct of type T in an array of data at a given index.
+pub fn get_mut_helper<T: bytemuck::Pod>(data: &mut [u8], index: DataIndex) -> &mut T {
+    let index_usize: usize = index as usize;
+    bytemuck::from_bytes_mut(&mut data[index_usize..index_usize + size_of::<T>()])
+}
+
+/// The standard `bool` is not a `Pod`, define a replacement that is
+/// https://docs.rs/spl-pod/latest/src/spl_pod/primitives.rs.html#13
+#[derive(Clone, Copy, Debug, Default, PartialEq, bytemuck::Pod, bytemuck::Zeroable)]
+#[repr(transparent)]
+pub struct PodBool(pub u8);
+impl PodBool {
+    pub const fn from_bool(b: bool) -> Self {
+        Self(if b { 1 } else { 0 })
+    }
+}
+
+impl From<bool> for PodBool {
+    fn from(b: bool) -> Self {
+        Self::from_bool(b)
+    }
+}
+
+#[test]
+fn test_pod_bool() {
+    assert_eq!(PodBool::from_bool(false).0 == 1, false);
+    assert_eq!(PodBool::from(false).0 == 1, false);
+}
+
+#[macro_export]
+macro_rules! trace {
+  ($($arg:tt)*) => {
+      #[cfg(feature = "trace")]
+      {
+        std::println!("[{}:{}] {}", std::file!(), std::line!(), std::format_args!($($arg)*));
+      }
+      #[cfg(feature = "trace-svm")]
+      {
+        solana_program::msg!("[{}:{}] {}", std::file!(), std::line!(), std::format_args!($($arg)*));
+      }
+  };
+}
