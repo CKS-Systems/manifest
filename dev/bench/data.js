b50ecf163eccfd9f1a0fc746745905a92004fcde
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 01:14:54 +0000

    add CU Benchmark (customSmallerIsBetter) benchmark result for 10678d196236c682c822b78c1c936a89237bead6

---
+++ dev/bench/data.js
@@ -0,0 +1,72 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724807694513,
+  "repoUrl": "https://github.com/CKS-Systems/manifest",
+  "entries": {
+    "CU Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Britt Cyr",
+            "username": "brittcyr",
+            "email": "cyrbritt@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "10678d196236c682c822b78c1c936a89237bead6",
+          "message": "22 in client (#33)",
+          "timestamp": "2024-08-28T00:25:24Z",
+          "url": "https://github.com/CKS-Systems/manifest/commit/10678d196236c682c822b78c1c936a89237bead6"
+        },
+        "date": 1724807694091,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PHX_50",
+            "value": 7021,
+            "unit": "CU",
+            "range": "",
+            "extra": ""
+          },
+          {
+            "name": "PHX_95",
+            "value": 13230,
+            "unit": "CU",
+            "range": "",
+            "extra": ""
+          },
+          {
+            "name": "PHX_99",
+            "value": 13938,
+            "unit": "CU",
+            "range": "",
+            "extra": ""
+          },
+          {
+            "name": "MFX_50",
+            "value": 7344,
+            "unit": "CU",
+            "range": "",
+            "extra": ""
+          },
+          {
+            "name": "MFX_95",
+            "value": 10717,
+            "unit": "CU",
+            "range": "",
+            "extra": ""
+          },
+          {
+            "name": "MFX_99",
+            "value": 12095,
+            "unit": "CU",
+            "range": "",
+            "extra": ""
+          }
+        ]
+      }
+    ]
+  }
+}
