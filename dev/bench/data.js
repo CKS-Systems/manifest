8d58317fce435fc2bd29f0daed967cb09dd6b8a0
github-action-benchmark <github@users.noreply.github.com>
2024-08-29 12:14:18 +0000

    add CU Benchmark (customSmallerIsBetter) benchmark result for 10252b80bb50cc17bc7f201257a30e5acdb79129

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,75 +1,11 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724930525156,
+  "lastUpdate": 1724933658224,
   "repoUrl": "https://github.com/CKS-Systems/manifest",
   "entries": {
     "CU Benchmark": [
       {
         "commit": {
           "author": {
-            "name": "Britt Cyr",
-            "username": "brittcyr",
-            "email": "cyrbritt@gmail.com"
-          },
-          "committer": {
-            "name": "GitHub",
-            "username": "web-flow",
-            "email": "noreply@github.com"
-          },
-          "id": "10678d196236c682c822b78c1c936a89237bead6",
-          "message": "22 in client (#33)",
-          "timestamp": "2024-08-28T00:25:24Z",
-          "url": "https://github.com/CKS-Systems/manifest/commit/10678d196236c682c822b78c1c936a89237bead6"
-        },
-        "date": 1724807694091,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "PHX_50",
-            "value": 7021,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "PHX_95",
-            "value": 13230,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "PHX_99",
-            "value": 13938,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "MFX_50",
-            "value": 7344,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "MFX_95",
-            "value": 10717,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "MFX_99",
-            "value": 12095,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "cyrbritt@gmail.com",
             "name": "Britt Cyr",
             "username": "brittcyr"
@@ -80,125 +16,59 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "a0284f2030b2b074af17a54f9bc6b5dd95f30330",
-          "message": "Make global only one level of tree (#30)\n\n* first step of overhauling global to be resistant to squatting\r\n\r\n* global cleanup\r\n\r\n* test fix",
-          "timestamp": "2024-08-28T18:13:30-04:00",
-          "tree_id": "b875573a46bef7e8406f85d9318b064aa394805c",
-          "url": "https://github.com/CKS-Systems/manifest/commit/a0284f2030b2b074af17a54f9bc6b5dd95f30330"
-        },
-        "date": 1724884222388,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "PHX_50",
-            "value": 7021,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "PHX_95",
-            "value": 13230,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "PHX_99",
-            "value": 13938,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "MFX_50",
-            "value": 7353,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "MFX_95",
-            "value": 10705,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          },
-          {
-            "name": "MFX_99",
-            "value": 12093,
-            "unit": "CU",
-            "range": "",
-            "extra": ""
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "cyrbritt@gmail.com",
-            "name": "Britt Cyr",
-            "username": "brittcyr"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "5448f5c293a76f1b731f0bc58a9b894312ce24b8",
-          "message": "More comments to clarify how rounding works (#39)\n\n* More comments to clarify how rounding works\r\n\r\n* fix merge",
-          "timestamp": "2024-08-28T18:33:08-04:00",
+          "id": "10252b80bb50cc17bc7f201257a30e5acdb79129",
+          "message": "Revert \"flip the sort order for red black tree iter (#41)\" (#44)\n\nThis reverts commit 4c94d8635e08b42f5bb3c8558d319d4a32c1b5f0.",
+          "timestamp": "2024-08-29T07:58:50-04:00",
           "tree_id": "6116489f146dd3c5c4fbbd006b5af4ddb7575283",
-          "url": "https://github.com/CKS-Systems/manifest/commit/5448f5c293a76f1b731f0bc58a9b894312ce24b8"
+          "url": "https://github.com/CKS-Systems/manifest/commit/10252b80bb50cc17bc7f201257a30e5acdb79129"
         },
-        "date": 1724885393263,
+        "date": 1724933657756,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
             "name": "PHX_50",
-            "value": 7021,
+            "value": 7003,
             "unit": "CU",
             "range": "",
             "extra": ""
           },
           {
             "name": "PHX_95",
-            "value": 13230,
+            "value": 13238,
             "unit": "CU",
             "range": "",
             "extra": ""
           },
           {
             "name": "PHX_99",
-            "value": 13938,
+            "value": 13922,
             "unit": "CU",
             "range": "",
             "extra": ""
           },
           {
             "name": "MFX_50",
-            "value": 7336,
+            "value": 7304,
             "unit": "CU",
             "range": "",
             "extra": ""
           },
           {
             "name": "MFX_95",
-            "value": 10671,
+            "value": 10386,
             "unit": "CU",
             "range": "",
             "extra": ""
           },
           {
             "name": "MFX_99",
-            "value": 12077,
+            "value": 12101,
             "unit": "CU",
             "range": "",
             "extra": ""
           }
         ]
-      },
+      }
     ]
   }
 }
