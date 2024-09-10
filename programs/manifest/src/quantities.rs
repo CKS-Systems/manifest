58ae7918529927a946805890205e4f962c37527a
Maximilian Schneider <mail@maximilianschneider.net>
2024-09-11 00:18:14 +0100

    adjust test boundary criteria

--- programs/manifest/src/quantities.rs
+++ programs/manifest/src/quantities.rs
@@ -551,9 +551,8 @@ fn test_price_limits() {
         QuoteAtomsPerBaseAtom::MAX_EXP
     )
     .is_ok());
-    // TODO: need to figure out why a lower MAX_EXP leads to worse performance
     assert!(QuoteAtomsPerBaseAtom::try_from_mantissa_and_exponent(
-        u32::MAX / 100,
+        u32::MAX,
         QuoteAtomsPerBaseAtom::MAX_EXP
     )
     .is_ok());
@@ -568,8 +567,7 @@ fn test_price_limits() {
     )
     .is_ok());
     assert!(QuoteAtomsPerBaseAtom::try_from(0f64).is_ok());
-    // TODO: need to figure out why D18 instead of D20 leads to worse performance
-    assert!(QuoteAtomsPerBaseAtom::try_from((u64::MAX / 100) as f64).is_ok());
+    assert!(QuoteAtomsPerBaseAtom::try_from(u64::MAX as f64).is_ok());
 
     // failures
     assert!(QuoteAtomsPerBaseAtom::try_from_mantissa_and_exponent(
