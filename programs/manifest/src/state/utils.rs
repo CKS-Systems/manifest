9e47d5658be7dbb12b4d6ee150059dcb15872428
Britt Cyr <cyrbritt@gmail.com>
2024-09-20 11:57:24 +0200

    make no clock tests be zero

--- programs/manifest/src/state/utils.rs
+++ programs/manifest/src/state/utils.rs
@@ -25,7 +25,7 @@ pub(crate) fn get_now_slot() -> u32 {
     // maliciously manipulated to clear all orders with expirations on the
     // orderbook.
     #[cfg(feature = "no-clock")]
-    let now_slot: u64 = super::NEXT_PLANNED_MAINTENANCE_SLOT as u32 - 1_u32;
+    let now_slot: u64 = (super::NEXT_PLANNED_MAINTENANCE_SLOT as u32 - 1_u32).into();
     #[cfg(not(feature = "no-clock"))]
     let now_slot: u64 = solana_program::clock::Clock::get()
         .unwrap_or(solana_program::clock::Clock {
