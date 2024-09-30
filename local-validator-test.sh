8d0b0846c7c9205aeed97bb71cc2f45a224aae1e
Britt Cyr <cyrbritt@gmail.com>
2024-09-30 18:25:44 +0200

    update local validator test

--- local-validator-test.sh
+++ local-validator-test.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
+cargo build-sbf
+echo "Rebuilt program"
+
 # Start a local validator
-solana-test-validator > /dev/null 2>&1 &
+solana-test-validator --bpf-program MNFSTqtC93rEfYHB6hF82sKdZpUDFWkViLByLd1k1Ms target/deploy/manifest.so --bpf-program UMnFStVeG1ecZFc2gc5K3vFy3sMpotq8C91mXBQDGwh target/deploy/ui_wrapper.so --bpf-program wMNFSTkir3HgyZTsB7uqu3i7FA73grFCptPXgrZjksL target/deploy/wrapper.so  >/dev/null 2>&1 &
 echo "Started test validator, sleeping for 15 seconds before starting"
 sleep 15
 
 # Save the pid so we can kill it at the end
 TEST_VALIDATOR_PID=$!
 
-cargo build-sbf
-echo "Rebuilt program"
-
 echo "Setting solana config to localnet"
 solana config set --url l
-
-solana program deploy target/deploy/manifest.so
-solana program deploy target/deploy/ui_wrapper.so
-solana program deploy target/deploy/wrapper.so
-echo "Deployed manifest and wrapper"
 
 yarn test
 echo "Done with client tests"
