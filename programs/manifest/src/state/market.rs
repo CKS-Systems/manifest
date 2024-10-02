240628f7352f3b3987b0cfecdcd53aa468b11185
Britt Cyr <cyrbritt@gmail.com>
2024-10-02 17:04:16 -0400

    Only get the next order when needed (#128)
    * move get next
    
    * rename
    
    * refactor

--- programs/manifest/src/state/market.rs
+++ programs/manifest/src/state/market.rs
@@ -613,14 +613,13 @@ impl<Fixed: DerefOrBorrowMut<MarketFixed>, Dynamic: DerefOrBorrowMut<[u8]>>
 
         let mut remaining_base_atoms: BaseAtoms = num_base_atoms;
         while remaining_base_atoms > BaseAtoms::ZERO && current_order_index != NIL {
-            let next_order_index: DataIndex =
-                get_next_candidate_match_index(fixed, dynamic, current_order_index, is_bid);
-
             let other_order: &RestingOrder =
                 get_helper::<RBNode<RestingOrder>>(dynamic, current_order_index).get_value();
 
             // Remove the resting order if expired.
             if other_order.is_expired(now_slot) {
+                let next_order_index: DataIndex =
+                    get_next_candidate_match_index(fixed, dynamic, current_order_index, is_bid);
                 remove_and_update_balances(
                     fixed,
                     dynamic,
@@ -693,6 +692,8 @@ impl<Fixed: DerefOrBorrowMut<MarketFixed>, Dynamic: DerefOrBorrowMut<[u8]>>
                     }),
                 )?;
                 if !has_enough_tokens {
+                    let next_order_index: DataIndex =
+                        get_next_candidate_match_index(fixed, dynamic, current_order_index, is_bid);
                     remove_and_update_balances(
                         fixed,
                         dynamic,
@@ -821,6 +822,8 @@ impl<Fixed: DerefOrBorrowMut<MarketFixed>, Dynamic: DerefOrBorrowMut<[u8]>>
                     remove_from_global(&global_trade_accounts_opt, &maker)?;
                 }
 
+                let next_order_index: DataIndex =
+                    get_next_candidate_match_index(fixed, dynamic, current_order_index, is_bid);
                 remove_order_from_tree_and_free(fixed, dynamic, current_order_index, !is_bid)?;
                 remaining_base_atoms = remaining_base_atoms.checked_sub(base_atoms_traded)?;
                 current_order_index = next_order_index;
