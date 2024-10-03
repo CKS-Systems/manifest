6f66498067244756b8945eaae55a8ba50d9da785
Britt Cyr <cyrbritt@gmail.com>
2024-10-03 13:28:48 -0400

    Update idl for anchor

--- client/idl/generateIdl.js
+++ client/idl/generateIdl.js
@@ -81,7 +81,9 @@ function modifyIdlCore(programName) {
           discriminator: [
             ...genLogDiscriminator(idl.metadata.address, idlAccount.name),
           ],
-          fields: idlAccount.type.fields,
+          fields: [
+              ...(idlAccount.type.fields).map((field) => { return { ...field, index: false };}),
+          ]
         };
         idl.events.push(event);
       }
@@ -223,6 +225,10 @@ function modifyIdlCore(programName) {
         }
       }
     }
+
+    // Return type has a tuple which anchor does not support
+    idl.types = idl.types.filter((idlType) => idlType.name != "BatchUpdateReturn");
+
   } else if (programName == 'ui_wrapper') {
     idl.types.push({
       name: 'OrderType',
@@ -252,7 +258,6 @@ function modifyIdlCore(programName) {
         });
       }
     }
-
 
     for (const instruction of idl.instructions) {
       switch (instruction.name) {
