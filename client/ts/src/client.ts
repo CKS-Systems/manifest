048484398a4626f2c4e5617bb15e7898c0dc1445
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-10-05 12:47:31 -0400

    Allow wrapper program to be passed down from getClientForMarket

--- client/ts/src/client.ts
+++ client/ts/src/client.ts
@@ -144,6 +144,7 @@ export class ManifestClient {
     connection: Connection,
     marketPk: PublicKey,
     payerKeypair: Keypair,
+    wrapperProgram: PublicKey,
   ): Promise<ManifestClient> {
     const marketObject: Market = await Market.loadFromAddress({
       connection: connection,
@@ -176,12 +177,15 @@ export class ManifestClient {
           owner: payerKeypair.publicKey,
           wrapperState: wrapperKeypair.publicKey,
         });
-      const claimSeatIx: TransactionInstruction = createClaimSeatInstruction({
-        manifestProgram: MANIFEST_PROGRAM_ID,
-        owner: payerKeypair.publicKey,
-        market: marketPk,
-        wrapperState: wrapperKeypair.publicKey,
-      });
+      const claimSeatIx: TransactionInstruction = createClaimSeatInstruction(
+        {
+          manifestProgram: MANIFEST_PROGRAM_ID,
+          owner: payerKeypair.publicKey,
+          market: marketPk,
+          wrapperState: wrapperKeypair.publicKey,
+        },
+        wrapperProgram,
+      );
       transaction.add(createAccountIx);
       transaction.add(createWrapperIx);
       transaction.add(claimSeatIx);
