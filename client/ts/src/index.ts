2aa4811195de74ee79f1846623ebd157bd3a620d
Maximilian Schneider <mail@maximilianschneider.net>
2024-10-16 23:22:41 +0300

    export utils

--- client/ts/src/index.ts
+++ client/ts/src/index.ts
@@ -2,6 +2,7 @@ export * from './client';
 export * from './market';
 export * from './types';
 export * as manifest from './manifest';
+export * as utils from './utils';
 export * as wrapper from './wrapper';
 export * from './wrapperObj';
 export * as uiWrapper from "./ui_wrapper";
