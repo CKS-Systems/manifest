c38c85503923bd225c25b38453db6dfdc2e54a88
Cody Lamson <tovarishfin@gmail.com>
2024-10-23 13:24:34 +0200

    Resilient fill feed (#214)
    * make fill feed more resilient
    
    * monitor activity and throw if none, stop the feed before restarting
    
    * add a way to monitor activity
    
    * only apply fill updates when fill is relevant market
    
    * lint fixes

--- debug-ui/app/components/Chart.tsx
+++ debug-ui/app/components/Chart.tsx
@@ -53,6 +53,9 @@ const Chart = ({ marketAddress }: { marketAddress: string }): ReactElement => {
 
     ws.onmessage = async (message): Promise<void> => {
       const fill: FillLogResult = JSON.parse(message.data);
+      if (fill.market !== marketAddress) {
+        return;
+      }
 
       const aggregateFillData = async (
         fill: FillLogResult,
@@ -123,7 +126,7 @@ const Chart = ({ marketAddress }: { marketAddress: string }): ReactElement => {
     return (): void => {
       ws.close();
     };
-  }, [chartEntries, conn]);
+  }, [chartEntries, conn, marketAddress]);
 
   useEffect(() => {
     if (chartContainerRef.current) {
