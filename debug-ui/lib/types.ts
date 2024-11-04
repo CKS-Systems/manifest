4615246b2abb984994f4b5604c3b2bc7efbcdd48
Britt Cyr <cyrbritt@gmail.com>
2024-11-04 17:43:16 -0500

    Handle duplicates in the debug UI

--- debug-ui/lib/types.ts
+++ debug-ui/lib/types.ts
@@ -2,6 +2,16 @@ export interface LabelsByAddr {
   [addr: string]: string;
 }
 
+// When true, this is the primary market for a given pair. This is defined by
+// quote volume traded and determines which is shown in UI.
+export interface ActiveByAddr {
+  [addr: string]: boolean;
+}
+
+export interface VolumeByAddr {
+  [addr: string]: number;
+}
+
 export type FillResultUi = {
   market: string;
   maker: string;
