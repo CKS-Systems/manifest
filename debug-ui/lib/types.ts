a03ce256deec9690132f2b27795e64a2a0ee22a1
Britt Cyr <cyrbritt@gmail.com>
2024-11-04 17:56:10 -0500

    comment'

--- debug-ui/lib/types.ts
+++ debug-ui/lib/types.ts
@@ -4,6 +4,7 @@ export interface LabelsByAddr {
 
 // When true, this is the primary market for a given pair. This is defined by
 // quote volume traded and determines which is shown in UI.
+// TODO: Make a toggle for show all
 export interface ActiveByAddr {
   [addr: string]: boolean;
 }
