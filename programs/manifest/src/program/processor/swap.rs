1264956b33b1c6ea7e8663c00d2d68be02484ccf
Britt Cyr <cyrbritt@gmail.com>
2024-11-07 04:01:15 +0100

    Check that in_atoms is backed up by actual funds

--- programs/manifest/src/program/processor/swap.rs
+++ programs/manifest/src/program/processor/swap.rs
@@ -83,6 +83,25 @@ pub(crate) fn process_swap(
     } = SwapParams::try_from_slice(data)?;
 
     trace!("swap in_atoms:{in_atoms} out_atoms:{out_atoms} is_base_in:{is_base_in} is_exact_in:{is_exact_in}");
+    
+    // This check is redundant with the check that will be done within token
+    // program on deposit, but it is done here to future proof in case we later
+    // remove checked math.
+    if is_exact_in {
+        if is_base_in {
+            require!(
+                in_atoms <= trader_base_account.get_balance_atoms(),
+                ManifestError::Overflow,
+                "Insufficient in atoms for swap"
+            )?;
+        } else {
+            require!(
+                in_atoms <= trader_quote_account.get_balance_atoms(),
+                ManifestError::Overflow,
+                "Insufficient in atoms for swap"
+            )?;
+        }
+    }
 
     // this is a virtual credit to ensure matching always proceeds
     // net token transfers will be handled later
