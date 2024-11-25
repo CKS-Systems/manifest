cf477db5f7bfb448942084ac2c50988a4396a98b
Britt Cyr <cyrbritt@gmail.com>
2024-11-25 12:59:52 -0500

    Fix typescript tests (#295)
    * Fix typescript tests
    
    * Fmt

--- client/ts/src/constants.ts
+++ client/ts/src/constants.ts
@@ -1,7 +1,8 @@
 export const FIXED_MANIFEST_HEADER_SIZE: number = 256;
 export const FIXED_GLOBAL_HEADER_SIZE: number = 96;
 export const FIXED_WRAPPER_HEADER_SIZE: number = 64;
-export const NIL = 4_294_967_295;
+// Changed in https://github.com/CKS-Systems/manifest/pull/294
+export const NIL: number = 0x7f_ff_ff_ff;
 export const NO_EXPIRATION_LAST_VALID_SLOT = 0;
 export const U32_MAX = 4_294_967_295;
 export const PRICE_MIN_EXP = -18;
