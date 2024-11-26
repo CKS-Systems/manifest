72c9b01d35e114f8ad72e51cb4c9c3b2699ef0bc
Britt Cyr <cyrbritt@gmail.com>
2024-11-26 16:28:24 -0500

    Update NIL (#300)
    * Update NIL
    
    * Fix nil

--- client/ts/src/constants.ts
+++ client/ts/src/constants.ts
@@ -1,8 +1,7 @@
 export const FIXED_MANIFEST_HEADER_SIZE: number = 256;
 export const FIXED_GLOBAL_HEADER_SIZE: number = 96;
 export const FIXED_WRAPPER_HEADER_SIZE: number = 64;
-// Changed in https://github.com/CKS-Systems/manifest/pull/294
-export const NIL: number = 0x7f_ff_ff_ff;
+export const NIL: number = 4_294_967_295;
 export const NO_EXPIRATION_LAST_VALID_SLOT = 0;
 export const U32_MAX = 4_294_967_295;
 export const PRICE_MIN_EXP = -18;
