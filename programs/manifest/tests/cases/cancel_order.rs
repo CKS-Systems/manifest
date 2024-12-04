fa01481cfb71dc7dffb148aaf5f8a8cffc744b51
Britt Cyr <cyrbritt@gmail.com>
2024-12-04 20:34:08 +0100

    fmt

--- programs/manifest/tests/cases/cancel_order.rs
+++ programs/manifest/tests/cases/cancel_order.rs
@@ -48,14 +48,18 @@ async fn cancel_order_bid_test() -> anyhow::Result<()> {
 async fn cancel_order_rounding_test() -> anyhow::Result<()> {
     let mut test_fixture: TestFixture = TestFixture::new().await;
     test_fixture.claim_seat().await?;
-    test_fixture.deposit(Token::USDC, 2 * USDC_UNIT_SIZE).await?;
+    test_fixture
+        .deposit(Token::USDC, 2 * USDC_UNIT_SIZE)
+        .await?;
 
     test_fixture
         .place_order(Side::Bid, 1, 11, -1, u32::MAX, OrderType::Limit)
         .await?;
 
     test_fixture.cancel_order(0).await?;
-    test_fixture.withdraw(Token::USDC, 2 * USDC_UNIT_SIZE).await?;
+    test_fixture
+        .withdraw(Token::USDC, 2 * USDC_UNIT_SIZE)
+        .await?;
 
     Ok(())
 }
