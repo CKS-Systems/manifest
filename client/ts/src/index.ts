418572895b7b30a5650e45ea457cbe518632fd0a
Britt Cyr <cyrbritt@gmail.com>
2025-01-14 17:07:54 +0000

    Scope exports intentionally

--- client/ts/src/index.ts
+++ client/ts/src/index.ts
@@ -2,9 +2,12 @@ export * from './client';
 export * from './market';
 export * from './global';
 export * from './types';
-export * as manifest from './manifest';
-export * as utils from './utils';
-export * as wrapper from './wrapper';
+
+// Do not export all of manifest because names collide with wrapper. Force users
+// to use the client.
+export * from './manifest/errors';
+export * from './manifest/accounts';
+export * from './wrapper/instructions';
+
 export * from './wrapperObj';
-export * as uiWrapper from './ui_wrapper';
 export * from './uiWrapperObj';
