42368560d65b785eb675d4e6bc8a0ea5972037e0
DonDuala <john@dual.finance>
2025-06-20 10:02:46 -0400

    refresh /metadata when new market gets fill

--- debug-ui/scripts/stats-server.ts
+++ debug-ui/scripts/stats-server.ts
@@ -330,7 +330,13 @@ export class ManifestStatsServer {
       });
 
       this.markets.set(market, marketObject);
-      return marketObject;
+      const metaplex: Metaplex = Metaplex.make(this.connection);
+      const baseSymbol = await this.lookupMintTicker(metaplex, marketObject.baseMint());
+      const quoteSymbol = await this.lookupMintTicker(metaplex, marketObject.quoteMint());
+      
+      this.tickers.set(market, [baseSymbol, quoteSymbol]);
+    
+    return marketObject;
     } catch (error) {
       console.error('Error loading market:', market, error);
       return undefined; // Changed from null to undefined
