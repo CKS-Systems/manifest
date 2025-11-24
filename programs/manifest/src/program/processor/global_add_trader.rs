aa8a01c28e2e51c3e164d5c8ae696cac0b5fa2ed
Britt Cyr <cyrbritt@gmail.com>
2025-11-24 19:37:09 +0000

    Drop borrows

--- programs/manifest/src/program/processor/global_add_trader.rs
+++ programs/manifest/src/program/processor/global_add_trader.rs
@@ -25,6 +25,23 @@ pub(crate) fn process_global_add_trader(
 
     let GlobalAddTraderContext { payer, global, .. } = global_add_trader_context;
 
+    {
+        // Charge a seat fee. This is stranded similar to forfeited global gas prepayments.
+        // This is necessary to prevent an attack where an attacker would claim a
+        // global seat and then delete their token account. In order for someone
+        // else to get that seat, they would need to init a token account for the
+        // attacker, giving them rent.
+        let rent: Rent = Rent::get()?;
+        invoke(
+            &solana_program::system_instruction::transfer(
+                &payer.key,
+                &global.key,
+                rent.minimum_balance(Account::LEN as usize) * 10,
+            ),
+            &[payer.info.clone(), global.info.clone()],
+        )?;
+    }
+
     // Needs a spot for this trader on the global account.
     expand_global(&payer, &global)?;
 
@@ -32,21 +49,6 @@ pub(crate) fn process_global_add_trader(
     let mut global_dynamic_account: GlobalRefMut = get_mut_dynamic_account(global_data);
 
     global_dynamic_account.add_trader(payer.key)?;
-
-    // Charge a seat fee. This is stranded similar to forfeited global gas prepayments.
-    // This is necessary to prevent an attack where an attacker would claim a
-    // global seat and then delete their token account. In order for someone
-    // else to get that seat, they would need to init a token account for the
-    // attacker, giving them rent.
-    let rent: Rent = Rent::get()?;
-    invoke(
-        &solana_program::system_instruction::transfer(
-            &payer.key,
-            &global.key,
-            rent.minimum_balance(Account::LEN as usize) * 10,
-        ),
-        &[payer.info.clone(), global.info.clone()],
-    )?;
 
     emit_stack(GlobalAddTraderLog {
         global: *global.key,
